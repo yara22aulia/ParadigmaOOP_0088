fe9546de74ad2bcded3fcda5189e6663c319589d
yara22aulia <yara22aulia@gmail.com>
2026-04-21 14:50:21 +0700

    first commit

---
+++ ParadigmaOOP.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa{
+public:
+    int nim;
+    string nama;
+    float nilai;
+    void printData(){
+        cout << "NIM   = " << nim << endl;
+        cout << "Nama  = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
+};
+
+int main() {
+    Mahasiswa mhs1;
+
+    mhs1.nim = 111;
+    mhs1.nama = "Joko Susilo";
+    mhs1.nilai = 75.5;
+    mhs1.printData();
+
+    return 0;
+}
