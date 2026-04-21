fc90a03f05dbbefd77bf7516820e2815f950f0da
yara22aulia <yara22aulia@gmail.com>
2026-04-21 15:08:22 +0700

    menambahkan fungsi main

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -8,10 +8,24 @@ public:
     string nama;
     float nilai;
     void printData
-    ()
-    {
+    (){
         cout << "NIM   = " << nim << endl;
         cout << "Nama  = " << nama << endl;
         cout << "Nilai = " << nilai << endl;
     }
 };
+
+int main(
+    
+)
+{
+    Mahasiswa mhs1;
+
+    mhs1.nim = 111;
+    mhs1.nama = "Joko Susilo";
+    mhs1.nilai = 75.5;
+
+    mhs1.printData();
+
+    return 0;
+}
