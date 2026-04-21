f069df5978897bc42ab4b6c65eec3b29405c8779
yara22aulia <yara22aulia@gmail.com>
2026-04-21 15:19:16 +0700

    menampilkan semua data mahasiswa

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -16,13 +16,12 @@ public:
 };
 
 int main
-()
-{
+(
+
+){
     Mahasiswa mhs1;
     mhs1.nim = 111;
     mhs1.nama = "Joko Susilo";
     mhs1.nilai = 75.5;
     mhs1.printData();
-
-    return 0;
 }
