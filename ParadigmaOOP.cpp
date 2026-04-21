86472c759509d25fe30c53534f5e110101866a2d
yara22aulia <yara22aulia@gmail.com>
2026-04-21 14:57:23 +0700

    membuat strukyur class dasar

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa{
+class Mahasiswa
+{
 public:
     int nim;
     string nama;
@@ -12,14 +13,3 @@ public:
         cout << "Nilai = " << nilai << endl;
     }
 };
-
-int main() {
-    Mahasiswa mhs1;
-
-    mhs1.nim = 111;
-    mhs1.nama = "Joko Susilo";
-    mhs1.nilai = 75.5;
-    mhs1.printData();
-
-    return 0;
-}
