302f8b23267255e77cdc24cb6af94bdaf15c351b
yara22aulia <yara22aulia@gmail.com>
2026-04-21 15:00:26 +0700

    menambahkan printData

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -7,7 +7,8 @@ public:
     int nim;
     string nama;
     float nilai;
-    void printData(){
+    void printData()
+    {
         cout << "NIM   = " << nim << endl;
         cout << "Nama  = " << nama << endl;
         cout << "Nilai = " << nilai << endl;
